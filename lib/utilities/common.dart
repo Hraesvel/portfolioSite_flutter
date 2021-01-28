d7015ddcc2f443799c641374d9793e8f9caded17
Martin Smith <mcodesmith@gmail.com>
2021-01-27 20:05:58 -0800

    updated utilities

--- lib/utilities/common.dart
+++ lib/utilities/common.dart
@@ -1,9 +1,15 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class CommonUtility {
 
   static Future<String> loadStringAsset(String path ) async {
     return rootBundle.loadString(path);
+  }
+
+  static Future<http.Response> fetchFromWeb(String url) async {
+    final res = await http.get(url);
+    return res;
   }
 }
