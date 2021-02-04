83dc4b7197ada392f36e23f15e4bfc6850fb7118
Martin Smith <mcodesmith@gmail.com>
2021-02-04 00:23:23 -0800

    update serialization for project type
    default serialization of project was not encoding the Unicode  (emojis) correctly this has been remedied with this change.

--- lib/types/project.dart
+++ lib/types/project.dart
@@ -1,24 +1,35 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
 part 'project.g.dart';
 
 @JsonSerializable()
 class Project {
-  Project(this.name,
+  Project(
+      List<String> tech,
+      this.name,
       this.description,
       this.link,
       this.image,
-      this.tech,
       this.priority,
-      this.bucket);
+      this.bucket)
+  {
+    this._tech = tech;
+  }
 
   int priority = 0;
   String name;
   String description;
   String link;
-  List<String> tech = [];
+  @JsonKey(name: 'tech')
+  List<String> _tech = [];
   String image;
   String thumb;
   String bucket;
+
+  List<String> get tech {
+    return _tech.map((e) => utf8.decode(e.codeUnits)).toList();
+  }
 
 
   factory Project.fromJson(Map<String, dynamic> json) => _$ProjectFromJson(json);
--- lib/types/project.g.dart
+++ lib/types/project.g.dart
@@ -8,11 +8,11 @@ part of 'project.dart';
 
 Project _$ProjectFromJson(Map<String, dynamic> json) {
   return Project(
+    (json['tech'] as List)?.map((e) => e as String)?.toList(),
     json['name'] as String,
     json['description'] as String,
     json['link'] as String,
     json['image'] as String,
-    (json['tech'] as List)?.map((e) => e as String)?.toList(),
     json['priority'] as int,
     json['bucket'] as String,
   )..thumb = json['thumb'] as String;
@@ -23,8 +23,8 @@ Map<String, dynamic> _$ProjectToJson(Project instance) => <String, dynamic>{
       'name': instance.name,
       'description': instance.description,
       'link': instance.link,
-      'tech': instance.tech,
       'image': instance.image,
       'thumb': instance.thumb,
       'bucket': instance.bucket,
+      'tech': instance.tech,
     };
