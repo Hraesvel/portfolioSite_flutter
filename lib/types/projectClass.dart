b6d8fa6214ab04c9b55fd409c09b6af1454835f8
Martin Smith <mcodesmith@gmail.com>
2021-02-09 18:09:42 -0800

    add async lib to deps

--- lib/types/projectClass.dart
+++ lib/types/projectClass.dart
@@ -7,25 +7,34 @@ part 'project.g.dart';
 class Project {
   Project(
       List<String> tech,
-      this.name,
-      this.description,
+      name,
+      description,
       this.link,
       this.image,
       this.priority,
       this.bucket)
   {
     this._tech = tech;
+    this._name = name;
+    this._description = description;
   }
 
   int priority = 0;
-  String name;
-  String description;
+
+  @JsonKey(name: 'name')
+  String _name;
+  @JsonKey(name: 'description')
+  String _description;
   String link;
+
   @JsonKey(name: 'tech')
   List<String> _tech = [];
   String image;
   String thumb;
   String bucket;
+
+  String get name => utf8.decode(_name.codeUnits);
+  String get description => utf8.decode(_description.codeUnits);
 
   List<String> get tech {
     return _tech.map((e) => utf8.decode(e.codeUnits)).toList();
---
+++ lib/types/projectClass.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'projectClass.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Project _$ProjectFromJson(Map<String, dynamic> json) {
+  return Project(
+    (json['tech'] as List)?.map((e) => e as String)?.toList(),
+    json['name'],
+    json['description'],
+    json['link'] as String,
+    json['image'] as String,
+    json['priority'] as int,
+    json['bucket'] as String,
+  )..thumb = json['thumb'] as String;
+}
+
+Map<String, dynamic> _$ProjectToJson(Project instance) => <String, dynamic>{
+      'priority': instance.priority,
+      'link': instance.link,
+      'image': instance.image,
+      'thumb': instance.thumb,
+      'bucket': instance.bucket,
+      'name': instance.name,
+      'description': instance.description,
+      'tech': instance.tech,
+    };
